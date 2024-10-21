af9fe3b70f8dc14c93a6b020a8f130c1a44d5cfa
novitasalsabila <novnovv10@gmail.com>
2024-10-21 17:49:32 +0700

    Layout

---
+++ app/src/main/java/com/example/pertemuan3/BelajarLayout.kt
@@ -0,0 +1,96 @@
+package com.example.pertemuan3
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource as painterResource
+
+
+@Composable
+fun BelajarLayout(
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.fillMaxSize()
+    ){
+        HeaderSection()
+        DetailMhs(
+            param = "Nama",
+            argu = "Novi"
+        )
+        DetailMhs(param = "Nim", argu = "20220140146")
+
+    }
+}
+
+@Composable
+fun HeaderSection(){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ){
+            Image(painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.clip(CircleShape)
+            )
+            Icon(
+                Icons.Filled.Check,
+                contentDescription =null,
+                modifier =Modifier.padding(30.dp)
+            )
+        }
+        Column(
+           modifier = Modifier.padding(14.dp)
+        ){
+            Text(text = "Teknologi Informasi")
+            Text(text = "Universitas Muhammadiyah Yogyakarta")
+        }
+
+
+
+    }
+}
+
+
+@Composable
+fun DetailMhs(
+    param : String, argu : String
+){
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(text = param,
+            modifier = Modifier.weight(0.8f))
+            Text(text = ":",
+            modifier = Modifier.weight(0.2f))
+            Text(text = argu,
+            modifier = Modifier.weight(2f))
+        }
+    }
+}
